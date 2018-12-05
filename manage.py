e25fdad30290e975c20aea55475ec4956b75a887
weblearn <yang@qq.com>
2018-12-05 14:19:36 +0800

    项目的配置及数据库的创建和sqlalchemy映射

--- manage.py
+++ manage.py
@@ -1,10 +1,22 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+
+class Config(object):
+    '''项目的配置'''
+    DEBUG = True
+
+    #为数据库添加配置
+    SQLALCHEMY_DATABASE_URI = "mysql://root:root@127.0.0.1:3306/webtest"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 app = Flask(__name__)
+
+# 加载配置
+app.config.from_object(Config)
 
 @app.route('/')
 def index():
     return 'index'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
