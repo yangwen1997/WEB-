7898f917e4f801a4e73162061251f24a67b785c6
weblearn <yang@qq.com>
2018-12-08 20:43:09 +0800

    集成scrip与数据库迁移关联配置

--- manage.py
+++ manage.py
@@ -3,6 +3,8 @@ from flask import Flask,session
 from flask.ext.sqlalchemy import SQLAlchemy     #建立数据库映射
 from flask.ext.wtf import CSRFProtect           #跨站伪造保护
 from flask_session import Session               #自定义session的库
+from flask_script import Manager                #从flask脚本中导入 管理包
+from flask_migrate import Migrate, MigrateCommand  #导入数据库迁移提交库和迁移库
 
 class Config(object):
     '''项目的配置'''
@@ -22,7 +24,7 @@ class Config(object):
        PERMANENT_SESSION_LIFETIME : session的过期时间    
     '''
     SESSION_TYPE = "redis"
-    SESSION_REDIS = StrictRedis(host=REDIS_HOST,port= REDIS_PORT)
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port= REDIS_PORT)
     SESSION_USE_SIGNER = True
     SESSION_PERMANENT = False
     PERMANENT_SESSION_LIFETIME = 84000 * 2
@@ -40,6 +42,12 @@ redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 CSRFProtect(app)
 #指定Session
 Session(app)
+#管理app
+manger = Manager(app)
+#进行数据库和app的迁移关联操作
+Migrate(app, db)
+#提交迁移操作
+manger.add_command('db', MigrateCommand)
 
 @app.route('/')
 def index():
@@ -48,4 +56,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run()
+    manger.run()
