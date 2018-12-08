a837a05c313d913b5f8868b67e05a78ecae9c6f1
weblearn <yang@qq.com>
2018-12-08 20:53:27 +0800

    分离配置到config文件

---
+++ config.py
@@ -0,0 +1,23 @@
+from redis import StrictRedis
+class Config(object):
+    '''项目的配置'''
+    DEBUG = True
+    #设置启动模式秘钥
+    SECRET_KEY = 'adO9TUW0KoxTuG+wstHqD59hvJAOexP6bIUROcrityGAdaSxkOvbKRIVdpcyttk1'
+
+    #为数据库添加配置
+    SQLALCHEMY_DATABASE_URI = "mysql://root:root@127.0.0.1:3306/webtest"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
+
+    #对Session进行配置
+    '''SESSION_TYPE : session的存贮类型  SESSION_REDIS：redis的ip端口 
+       SESSION_USE_SIGNER : 是否开启签名 SESSION_PERMANENT :是否永久保存
+       PERMANENT_SESSION_LIFETIME : session的过期时间    
+    '''
+    SESSION_TYPE = "redis"
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port= REDIS_PORT)
+    SESSION_USE_SIGNER = True
+    SESSION_PERMANENT = False
+    PERMANENT_SESSION_LIFETIME = 84000 * 2
--- manage.py
+++ manage.py
@@ -5,29 +5,8 @@ from flask.ext.wtf import CSRFProtect           #跨站伪造保护
 from flask_session import Session               #自定义session的库
 from flask_script import Manager                #从flask脚本中导入 管理包
 from flask_migrate import Migrate, MigrateCommand  #导入数据库迁移提交库和迁移库
+from config import Config
 
-class Config(object):
-    '''项目的配置'''
-    DEBUG = True
-    #设置启动模式秘钥
-    SECRET_KEY = 'adO9TUW0KoxTuG+wstHqD59hvJAOexP6bIUROcrityGAdaSxkOvbKRIVdpcyttk1'
-
-    #为数据库添加配置
-    SQLALCHEMY_DATABASE_URI = "mysql://root:root@127.0.0.1:3306/webtest"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    REDIS_HOST = '127.0.0.1'
-    REDIS_PORT = 6379
-
-    #对Session进行配置
-    '''SESSION_TYPE : session的存贮类型  SESSION_REDIS：redis的ip端口 
-       SESSION_USE_SIGNER : 是否开启签名 SESSION_PERMANENT :是否永久保存
-       PERMANENT_SESSION_LIFETIME : session的过期时间    
-    '''
-    SESSION_TYPE = "redis"
-    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port= REDIS_PORT)
-    SESSION_USE_SIGNER = True
-    SESSION_PERMANENT = False
-    PERMANENT_SESSION_LIFETIME = 84000 * 2
 
 app = Flask(__name__)
 
